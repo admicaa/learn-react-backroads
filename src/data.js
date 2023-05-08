1eea9e8426a396d241c5929ae2e43f508e64a125
admica <almestaadmica@gmail.com>
2023-05-08 21:59:56 +0300

    ABOUT, SERVICES AND TOURS COMPOENNT

--- src/data.js
+++ src/data.js
@@ -37,3 +37,69 @@ export const socialLinks = [
     href: "https://www.twitter.com",
   },
 ];
+
+export const services = [
+  {
+    id: 1,
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    icon: "fas fa-wallet fa-fw",
+  },
+  {
+    id: 2,
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    icon: "fas fa-tree fa-fw",
+  },
+  {
+    id: 3,
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    icon: "fas fa-socks fa-fw",
+  },
+];
+
+export const toursFunc = async () => {
+  return [
+    {
+      id: 1,
+      image: await import("./images/tour-1.jpeg"),
+      date: "august 26th, 2020",
+      title: "Tibet Adventure",
+      info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+      location: "china",
+      duration: 6,
+      cost: 2100,
+    },
+    {
+      id: 2,
+      image: await import("./images/tour-2.jpeg"),
+      date: "october 1th, 2020",
+      title: "best of java",
+      info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+      location: "indonesia",
+      duration: 11,
+      cost: 1400,
+    },
+    {
+      id: 3,
+      image: await import("./images/tour-3.jpeg"),
+      date: "september 15th, 2020",
+      title: "explore hong kong",
+      info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+      location: "hong kong",
+      duration: 8,
+      cost: 5000,
+    },
+    {
+      id: 4,
+      image: await import("./images/tour-4.jpeg"),
+      date: "december 5th, 2019",
+      title: "kenya highlights",
+      info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+      location: "kenya",
+      duration: 20,
+      cost: 3300,
+    },
+  ];
+};
