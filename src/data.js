3619e353b33b2b4bae90de8a2942ece3b770f0b5
admica <almestaadmica@gmail.com>
2023-05-08 21:33:49 +0300

    LINKS AS DATA

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const pageLinks = [
+  {
+    id: 1,
+    href: "#home",
+    text: "Home",
+  },
+  {
+    id: 2,
+    href: "#about",
+    text: "about",
+  },
+  {
+    id: 3,
+    href: "#services",
+    text: "services",
+  },
+  {
+    id: 4,
+    href: "#tours",
+    text: "tours",
+  },
+];
+export const socialLinks = [
+  {
+    id: 1,
+    icon: "fa-facebook",
+    href: "https://www.twitter.com",
+  },
+  {
+    id: 2,
+    icon: "fa-twitter",
+    href: "https://www.twitter.com",
+  },
+  {
+    id: 3,
+    icon: "fa-squarespace",
+    href: "https://www.twitter.com",
+  },
+];
